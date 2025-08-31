73ab5bc3a713b374b1670eca3794e504e46d7b60
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2025-08-31 11:34:27 +0530

    Time: 1 ms (84.03%), Space: 42.4 MB (76.66%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int Greatest = Integer.MIN_VALUE;
         ArrayList<Boolean> list = new ArrayList <>();
-        for(int i =0; i< candies.length; i++ ){
-            if(Greatest< candies[i]){
-                Greatest = candies[i];
+        int max = 0;
+        for(int i = 0; i < candies.length; i++){
+            if(max < candies[i]){
+                max = candies[i];
             }
         }
-        int maxCandie = 0;
-        int sum = 0;
         for(int i = 0; i < candies.length; i++){
-            sum = candies[i] + extraCandies ;
-            maxCandie = sum;
-            if(maxCandie >= Greatest){
-               list.add(true);
+            if(candies[i]+extraCandies >= max){
+                list.add(true);
             }
             else{
                 list.add(false);
             }
-            sum = 0;
         }
         return list;
+
     }
 }
