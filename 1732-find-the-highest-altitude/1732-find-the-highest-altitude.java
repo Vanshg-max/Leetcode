a579e410b5e1975eb11a0d12eb1500f264ed35ab
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2025-09-01 18:23:02 +0530

    Time: 0 ms (100%), Space: 41.1 MB (86.85%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,13 +1,13 @@
 class Solution {
     public int largestAltitude(int[] gain) {
         
-        int currAltitude = 0;
-        int maxAltitude =0;
+        int sum = 0;
+        int maxSum =0;
         for(int i = 0; i < gain.length; i++){
-           currAltitude += gain[i];
-           maxAltitude = Math.max(maxAltitude,currAltitude);
+            sum = gain[i]+sum;
+            maxSum = Math.max(maxSum,sum);
         }
-        return maxAltitude;
+        return maxSum;
 
         
     }
