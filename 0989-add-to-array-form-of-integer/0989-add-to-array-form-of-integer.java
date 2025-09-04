d53fd66d49ac6f2b119face025930d2138ee7c50
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2025-09-04 21:15:27 +0530

    Time: 55 ms (22.31%), Space: 45.6 MB (88.81%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = num.length-1; //start from last digit of num
+
+        // loop until both digit from num and k are finished
+        while(i>=0 || k>0)
+        {
+            // if there is still a digit left in num add it to k
+            if(i>=0){
+                k += num[i];
+                i--;// move to the next left digit
+            }
+            // Get the last digit of k ->this becomes part of result
+            list.add(0,k%10);
+            // Carry->remove the last digit from k
+            k /=10;
+        }
+        return list;
+
+        
+    }
+}
