6c2904506ad158459665e1baff85146dd70e032f
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2025-09-12 19:06:09 +0530

    Time: 0 ms (100%), Space: 40.5 MB (49.49%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -9,7 +9,7 @@
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int start = 0 ;
+        int start = 1 ;
         int end = n;
         while(start<=end){
             int mid = start + (end - start) / 2 ;
