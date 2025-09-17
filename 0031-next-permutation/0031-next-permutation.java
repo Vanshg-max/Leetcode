54864206b0801c053eaa67a67e55aac52271b009
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2025-09-17 19:25:46 +0530

    Time: 0 ms (100%), Space: 43.3 MB (21.62%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+       
+       int n = nums.length;
+       int i = n-2;
+        //get breakpoint
+       while(i >= 0 &&  nums[i] >= nums[i+1]){
+        i--;
+       }
+        int j = n-1;
+       if(i>=0){
+       
+        while(nums[j]<=nums[i]){
+        j--;
+        }
+        swap(nums,i,j);
+       }
+
+       reverse(nums,i+1,n-1);
+
+        
+    }
+
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+}
