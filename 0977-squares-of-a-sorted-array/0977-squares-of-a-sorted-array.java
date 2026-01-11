b90d0e482b0df141707e8f9793e597131194e1d4
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-01-11 11:20:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,31 +1,40 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        List<Integer> neg = new ArrayList<>();
+        // Creata 2 list 
         List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
 
-        // Separate negative and positive numbers
-        for (int num : nums) {
-            if (num < 0)
-                neg.add(num);
-            else
+        // check pos and neg then add 
+        for(int num:nums){
+            if(num < 0){
+                neg.add (num);
+            }
+            else{
                 pos.add(num);
+            }
         }
 
-        // Case 1: No negative numbers
-        if (neg.size() == 0) {
-            for (int i = 0; i < pos.size(); i++)
-                pos.set(i, pos.get(i) * pos.get(i));
-            return pos.stream().mapToInt(Integer::intValue).toArray();
+        // Check fo ronly neg
+        if(pos.size()==0){
+            for(int i = 0; i<neg.size(); i++){
+                neg.set(i,neg.get(i)*neg.get(i));
+                Collections.reverse(neg);
+                
+            }
+             return neg.stream().mapToInt(Integer::intValue).toArray();
         }
 
-        // Case 2: No positive numbers
-        if (pos.size() == 0) {
-            for (int i = 0; i < neg.size(); i++)
-                neg.set(i, neg.get(i) * neg.get(i));
-            Collections.reverse(neg);
-            return neg.stream().mapToInt(Integer::intValue).toArray();
+
+        // Check for only pos
+        if(neg.size()==0){
+            for(int i = 0; i<pos.size(); i++){
+                pos.set(i,pos.get(i)*pos.get(i));
+                
+            }
+             return pos.stream().mapToInt(Integer::intValue).toArray();
         }
+
+       
 
         // Case 3: Both negative and positive exist
         int i = 0, j = 0, id = 0;
@@ -64,4 +73,8 @@ class Solution {
 
         return res;
     }
-}
+
+
+
+        
+    }
