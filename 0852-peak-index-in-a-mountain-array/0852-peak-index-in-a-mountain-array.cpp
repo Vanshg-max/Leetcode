89c69bc95d1a380819457885d7de2b7c5a3d2735
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-01-21 17:11:25 +0530

    Time: 0 ms (100%), Space: 63.5 MB (77.4%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,19 +4,23 @@ public:
         int s= 0;
         int e = size(arr)-1;
 
-        int mid = (s + (e-s)) /2;
-
+        int mid=0;
+        int ans;
         while (s <= e){
+                      mid = (s + e) /2;
+
             if(arr[mid] >arr[mid+1] &&  arr[mid] > arr[mid-1]){
 
-                return mid;
+                ans = mid;
+                return ans;
             }
-            else if( arr[mid] > arr[mid+1]){
+            else if( arr[mid] < arr[mid+1]){
                 s = mid+1;
             }
             else{
                 e = mid-1;
             }
+          
         }
         return s;
         
