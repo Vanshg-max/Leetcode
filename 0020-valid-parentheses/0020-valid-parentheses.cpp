8cdaf482d1d18698cdd6ca735abe64532eea2e68
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-02-16 15:23:25 +0530

    Time: 3 ms (9.59%), Space: 8.8 MB (66.98%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stk.push(ch);
+
+            } else {
+                if (!stk.empty()) {
+                    char top = stk.top();
+                    if ((top == '(' && ch == ')'  ) ||
+                        (top == '[' && ch == ']'  ) ||
+                        (top == '{' && ch == '}'  )) {
+                        stk.pop();
+                    } else {
+                        return false;
+                    }
+                } 
+                else{
+                    return false;
+                }
+            }
+        }
+        if(stk.empty()){
+            return true;
+        }
+        return false;
+        
+        
+    }
+};
