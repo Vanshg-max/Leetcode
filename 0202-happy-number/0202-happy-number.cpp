6dacaa399f5e86d44f66059e3ab227921bb4f817
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-03-05 01:32:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.28%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int square(int num){
+        int ans =0;
+        while(num > 0){
+           int x = num % 10;
+            ans += x*x;
+            num = num /10;
+
+        }
+        return ans;
+
+    }
+    bool isHappy(int n) {
+        int slow = square(n);
+        int fast = square(square(n));
+        while(slow != fast){
+            slow = square(slow);
+            fast =square(square(fast));
+            if(slow == 1 || fast == 1) return true;
+        }
+        return slow== 1;
+    }
+
+};
