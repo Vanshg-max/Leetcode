08c106c70b1e5bba7957694ee3202882162704de
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-03-25 09:40:49 +0530

    Time: 9 ms (55.4%), Space: 126.5 MB (74.1%) - LeetHub

---
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        long long totalSum = 0;
+
+        // 1. Calculate Total Sum
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                totalSum += grid[i][j];
+            }
+        }
+
+        // If total is odd, we can't split it equally into integers
+        if (totalSum % 2 != 0) return false;
+        long long target = totalSum / 2;
+
+        
+        long long currentPrefixSum = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                currentPrefixSum += grid[i][j];
+            }
+            if (currentPrefixSum == target) return true;
+        }
+
+        
+        currentPrefixSum = 0;
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < rows; i++) {
+                currentPrefixSum += grid[i][j];
+            }
+            if (currentPrefixSum == target) return true;
+        }
+
+        return false;
+    }
+};
