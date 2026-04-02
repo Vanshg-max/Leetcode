304a9451c153212e7b14690e4b5075f95896dde2
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-02 11:12:34 +0530

    Time: 1 ms (44.87%), Space: 10.6 MB (7.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-        s.erase(remove_if(s.begin(), s.end(), [](unsigned char c) {
-            return !isalnum(c); 
-        }), s.end());
-
-        int i = 0;
-        int j = s.length()-1;
-        while(i<j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
+        string clean = "";
+        for(char c : s){
+            if(isalnum(c)){
+                clean += tolower(c);
+            }
         }
-        return true;
+        string temp = clean;
+        reverse(clean.begin(),clean.end());
+        return  temp == clean ;
         
     }
 };
