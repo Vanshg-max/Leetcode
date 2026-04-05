18fd5d600fb2b3e50cc147cfdee8a766cc7ca465
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-05 10:55:02 +0530

    Time: 0 ms (100%), Space: 13.3 MB (68.96%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int solveRec(int n, vector<int> &days, vector<int > &costs, int index){
+        if(index >= n) return 0;
+
+        //for 1 day
+        int option1 = costs[0] + solveRec(n, days, costs, index + 1);
+        //for 7 days
+        int i ;
+        for( i = index; i<n && days[i] < days[index]+7; i++);
+        int option2 = costs[1] + solveRec(n,days,costs,i);
+        //for 30 days
+         for( i = index; i<n && days[i] < days[index]+30; i++);
+        int option3 = costs[2] + solveRec(n,days,costs,i);
+
+        return min(option1, min(option2,option3));
+
+    }
+
+    // using memoization
+     int solveMem(int n, vector<int> &days, vector<int > &costs, int index,vector<int> &dp){
+        if(index >= n) return 0;
+
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        //for 1 day
+        int option1 = costs[0] + solveMem(n, days, costs, index + 1,dp);
+        //for 7 days
+        int i ;
+        for( i = index; i<n && days[i] < days[index]+7; i++);
+        int option2 = costs[1] + solveMem(n,days,costs,i,dp);
+        //for 30 days
+         for( i = index; i<n && days[i] < days[index]+30; i++);
+        int option3 = costs[2] + solveMem(n,days,costs,i,dp);
+
+        dp[index] =min(option1, min(option2,option3));
+         return dp[index];
+
+    }
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n = days.size();
+        // return solveRec(n,days,costs,0);
+        vector<int > dp(n+1,-1);
+        return solveMem(n,days,costs,0,dp);
+        
+    }
+};
