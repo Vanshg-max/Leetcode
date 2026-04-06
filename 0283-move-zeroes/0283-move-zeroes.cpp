ca0cdba30aaf550a6f1078303d020c56c6447b13
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-06 16:26:41 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i =0;
-            for (int j = 0; j < nums.size(); j++){
-                if(nums[j] != 0 ){
-                    //swap
-                    swap(nums[j],nums[i]);
-                    i++;
-                }
+        int zero = 0;
+        for(int j=0; j<nums.size();j++){
+            if(nums[j] != 0){
+                swap(nums[zero], nums[j]);
+                zero++;
             }
-        
-    
+        }
         
     }
 };
