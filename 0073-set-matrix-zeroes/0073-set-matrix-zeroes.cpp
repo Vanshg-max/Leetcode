2e8df272c8c75d2fd8b0d31e2f35c9eb8f539c9f
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-09 14:52:01 +0530

    Time: 35 ms (5.6%), Space: 21.3 MB (8.81%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<vector<int>> temp(matrix);
+        for(int i=0 ; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                if(matrix[i][j] == 0 ){
+                    //make row and col of matrix zer0;
+                    for(int k = 0; k<matrix[0].size(); k++){
+                        temp[i][k] = 0;
+                    }
+                    for(int k = 0; k <matrix.size(); k++){
+                        temp[k][j]=0;
+                    }
+                }
+            }
+        }
+        matrix = temp;
+        
+    }
+};
