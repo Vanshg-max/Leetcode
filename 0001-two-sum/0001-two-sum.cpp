077dd6cc4005e6a39a452d1ad4e7fbcae51aef89
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-09 10:34:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>> valWithIdx;
-        for(int i = 0; i<nums.size(); i++){
-            valWithIdx.push_back({nums[i],i});
+        //using map
+    unordered_map<int,int>mp;
+    for(int i = 0; i<nums.size(); i++){
+        int complement = target - nums[i];
+        if(mp.count(complement)){
+            return {mp[complement],i};
         }
+        mp[nums[i]] = i;
+    }
+    return {};
 
-        sort(valWithIdx.begin(),valWithIdx.end());
-
-        int st = 0, en = nums.size()-1;
-        while(st < en){
-            int currSum = valWithIdx[st].first+valWithIdx[en].first;
-            if(currSum == target){
-                return {valWithIdx[st].second,valWithIdx[en].second};
-            }
-            else if(currSum > target){
-                en--;
-            }
-            else{
-                st++;
-            }
-
-
-        }
         
-        return {};
     }
 };
