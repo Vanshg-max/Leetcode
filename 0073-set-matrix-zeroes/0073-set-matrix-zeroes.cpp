949345d18b6b8d9abeb153bf847e767f148c549a
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-09 15:02:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> temp(matrix);
-        for(int i=0 ; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                if(matrix[i][j] == 0 ){
-                    //make row and col of matrix zer0;
-                    for(int k = 0; k<matrix[0].size(); k++){
-                        temp[i][k] = 0;
-                    }
-                    for(int k = 0; k <matrix.size(); k++){
-                        temp[k][j]=0;
-                    }
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        bool row0=false,col0=false;
+
+        for(int j = 0; j<m; j++){
+            if(matrix[0][j] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+        for(int j = 0; j<n; j++){
+            if(matrix[j][0] == 0){
+                col0 = true;
+            }
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        matrix = temp;
-        
+
+        for(int i =1; i<n; i++){
+            for(int j =1; j <m;j++){
+                if(matrix[i][0] ==0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(row0){
+            for(int j = 0; j <m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+         if(col0){
+            for(int i = 0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
