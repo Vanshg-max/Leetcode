a18771ac0c16d039017a06c0538c04d327282bce
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-20 22:56:05 +0530

    Time: 0 ms (100%), Space: 12.2 MB (6.97%) - LeetHub

---
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int maxi = 0;
+
+        for(int i = n-1; i>=0; i--){
+            if(colors[i]  != colors[0]){
+                maxi = max(maxi,i);
+                break;
+            }
+        }
+        for(int i = 0; i<n; i++){
+            if(colors[i]  != colors[n-1]){
+                maxi = max(maxi,n-1-i);
+                break;
+            }
+        }
+        return maxi;
+        
+    }
+};
