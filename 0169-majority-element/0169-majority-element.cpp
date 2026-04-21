9cc8926d4206af324988205b5179454a89b0b394
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-21 11:22:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 1;
-        int i =0;
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-
-        while(i < n-1){
-            if(nums[i] == nums[i+1]){
+        int count = 0;
+        int candidate;
+        for(int num: nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate){
                 count++;
-                if(count > n/2){
-                    return nums[i];
-                }
-
             }
             else{
-                count = 1;
+                count--;
             }
-            i++;
-
         }
-        return nums[0];
+        return candidate;
         
     }
 };
