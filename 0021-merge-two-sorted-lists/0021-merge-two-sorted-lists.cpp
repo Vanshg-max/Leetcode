437c778cfbb0ce530a5f6b99b7a3812dc943c163
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-04-29 11:18:36 +0530

    Time: 0 ms (100%), Space: 19.4 MB (86.99%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,51 +10,26 @@
  */
 class Solution {
 public:
-  
-    ListNode* solve(ListNode* first, ListNode* second) {
-       if (first->next == NULL) {
-        first->next = second;
-        return first;
-    }
-       ListNode* curr1 = first;
-    ListNode* next1 = curr1->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode (-1);
 
-    ListNode* curr2 = second;
-    ListNode* next2 = curr2->next;
+        ListNode* temp = dummy;
 
-    
-
-    while (next1 != NULL && curr2 != NULL) {
-        if ((curr2->val >= curr1->val) && (curr2->val <= next1->val)) {
-            curr1->next = curr2;
-            curr2->next = next1;
-            curr1 = curr2;
-            curr2 = next2;
-        } else {
-            curr1 = next1;
-            next1 = next1->next;
-            if (next1 == NULL) {
-                curr1->next = curr2;
-                return first;
+        while(list1 != NULL && list2 != NULL){
+            if(list1 -> val < list2 -> val){
+                temp -> next = list1;
+                list1 = list1 -> next;
             }
-        }
-        if (curr2 != NULL) {
-            next2 = curr2->next;
-        }
-    }
-
-    return first;
-    }
-    public:
-        ListNode* mergeTwoLists(ListNode * list1, ListNode * list2) {
-            if (list1 == NULL)
-                return list2;
-            if (list2 == NULL)
-                return list1;
-            if (list1->val <= list2->val)
-                return solve(list1, list2);
-            else {
-                return solve(list2, list1);
+            else{
+                temp -> next = list2;
+                list2 = list2 ->next;
             }
+            temp = temp -> next;
         }
-    };
+        if(list1)  temp -> next = list1;
+        if(list2)  temp -> next = list2;
+
+        return dummy->next;
+        
+    }
+};
