785d0f6c026eee6113996989f46f8c68b7166206
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-05-03 08:37:29 +0530

    Time: 0 ms (100%), Space: 8 MB (97.22%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        for(int i = 0; i< s.size(); i++){
+            char temp = s[0];
+            s.erase(0,1);
+              s+= temp;
+            if(s == goal) return true;
+        }
+        return false;
+    }
+};
