d9bc7733be54b069891b9a5260a82c9f1d02d6db
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-05-03 08:44:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        for(int i = 0; i< s.size(); i++){
-            char temp = s[0];
-            s.erase(0,1);
-              s+= temp;
-            if(s == goal) return true;
+
+        if(s.size() != goal.size()){
+            return false;
         }
-        return false;
+
+        string temp = s + s;
+
+        return temp.find(goal) != string::npos;
+        
     }
 };
