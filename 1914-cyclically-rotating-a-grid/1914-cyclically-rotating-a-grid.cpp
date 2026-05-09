0c5dbdc44d3588380adeca6489ff07dd82b2a246
Vanshg-max <vanshgaikwadpersonal@gmail.com>
2026-05-09 22:08:01 +0530

    Time: 14 ms (57.35%), Space: 18.3 MB (16.18%) - LeetHub

---
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+        
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int layers = min(m, n) / 2;
+
+        for(int layer = 0; layer < layers; layer++) {
+
+            vector<int> arr;
+
+            int top = layer;
+            int left = layer;
+            int bottom = m - layer - 1;
+            int right = n - layer - 1;
+
+            // top row
+            for(int j = left; j <= right; j++) {
+                arr.push_back(grid[top][j]);
+            }
+
+            // right column
+            for(int i = top + 1; i <= bottom - 1; i++) {
+                arr.push_back(grid[i][right]);
+            }
+
+            // bottom row
+            for(int j = right; j >= left; j--) {
+                arr.push_back(grid[bottom][j]);
+            }
+
+            // left column
+            for(int i = bottom - 1; i >= top + 1; i--) {
+                arr.push_back(grid[i][left]);
+            }
+
+            int len = arr.size();
+            int rot = k % len;
+
+            vector<int> rotated(len);
+
+            // counter-clockwise rotation
+            for(int i = 0; i < len; i++) {
+                rotated[i] = arr[(i + rot) % len];
+            }
+
+            int idx = 0;
+
+            // fill top row
+            for(int j = left; j <= right; j++) {
+                grid[top][j] = rotated[idx++];
+            }
+
+            // fill right column
+            for(int i = top + 1; i <= bottom - 1; i++) {
+                grid[i][right] = rotated[idx++];
+            }
+
+            // fill bottom row
+            for(int j = right; j >= left; j--) {
+                grid[bottom][j] = rotated[idx++];
+            }
+
+            // fill left column
+            for(int i = bottom - 1; i >= top + 1; i--) {
+                grid[i][left] = rotated[idx++];
+            }
+        }
+
+        return grid;
+    }
+};
